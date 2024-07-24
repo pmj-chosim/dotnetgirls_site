ff1b5caaac25d767b97fda213b8ffef38a85ab95
박민진 <114579651+pmj-chosim@users.noreply.github.com>
2024-07-24 13:23:12 +0000

    workspace template

--- dotnetgirls_site/obj/Debug/net8.0/dotnetgirls_site.AssemblyInfo.cs
+++ dotnetgirls_site/obj/Debug/net8.0/dotnetgirls_site.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnetgirls_site")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c345689c966ec1dc1495cb9de0cdad2420c16258")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+908f2a8adf478f8e79581205c99814cbfbf3338b")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnetgirls_site")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnetgirls_site")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
